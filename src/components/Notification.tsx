60d09bdf061ee6ea23810dd1ac7b92bbcfe1e114
“Lissa <lwarshaw674@gmail.com>
2023-09-23 15:28:47 -0400

    logos

--- src/components/Notification.tsx
+++ src/components/Notification.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Notification = () => {
   return (
-    <div className='h-12 bg-yellow-200 text-orange-500 px-4 flex items-center justify-center'>Welcome To Dropwhich!</div>
+    <div className='h-12 bg-yellow-200 text-orange-500 px-4 flex items-center justify-center text-center text-sm md:text-base cursor-pointer'>Welcome To Dropwhich!</div>
   )
 }
 

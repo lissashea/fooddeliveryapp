8198194b764670a3f05b78825e8cf56ae41623b4
“Lissa <lwarshaw674@gmail.com>
2023-09-23 14:22:43 -0400

    notifications

---
+++ src/components/Notification.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Notification = () => {
+  return (
+    <div className='h-12 bg-yellow-200 text-orange-500 px-4 flex items-center justify-center'>Welcome To Dropwhich!</div>
+  )
+}
+
+export default Notification

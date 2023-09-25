4c555438170ba00c07570f767d1e658b2ff68282
“Lissa <lwarshaw674@gmail.com>
2023-09-25 14:46:20 -0400

    admin config

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,3 @@
-
 import Featured from '@/components/Featured'
 import Offer from '@/components/Offer'
 import Slider from '@/components/Slider'
--- src/data.ts
+++ src/data.ts
@@ -1,393 +1,393 @@
-type Product = {
-  id: number;
-  title: string;
-  desc?: string;
-  img?: string;
-  price: number;
-  options?: { title: string; additionalPrice: number }[];
-};
+// type Product = {
+//   id: number;
+//   title: string;
+//   desc?: string;
+//   img?: string;
+//   price: number;
+//   options?: { title: string; additionalPrice: number }[];
+// };
 
-type Products = Product[];
+// type Products = Product[];
 
-export const featuredProducts: Products = [
-  {
-    id: 1,
-    title: "Sicilian",
-    desc: "Ignite your taste buds with a fiery combination of spicy pepperoni, jalapeños, crushed red pepper flakes, and melted mozzarella cheese, delivering a kick with every bite.",
-    img: "/temporary/drop1.png",
-    price: 24.9,
-    options: [
-      {
-        title: "Small",
-        additionalPrice: 0,
-      },
-      {
-        title: "Medium",
-        additionalPrice: 4,
-      },
-      {
-        title: "Large",
-        additionalPrice: 6,
-      },
-    ],
-  },
-  {
-    id: 2,
-    title: "Sicilian",
-    desc: "Indulge in smoky goodness with a flame-grilled beef patty, topped with crispy bacon, melted cheddar cheese, caramelized onions, and a smattering of tangy BBQ sauce.",
-    img: "/temporary/drop2.png",
-    price: 29.9,
-    options: [
-      {
-        title: "Small",
-        additionalPrice: 0,
-      },
-      {
-        title: "Medium",
-        additionalPrice: 4,
-      },
-      {
-        title: "Large",
-        additionalPrice: 6,
-      },
-    ],
-  },
-  {
-    id: 3,
-    title: "tbd",
-    desc: "A classic Italian delight featuring a thin, crispy crust, tangy tomato sauce, fresh mozzarella, and a medley of aromatic herbs topped with lettuce, tomatoes, and a dollop of tangy mayo.",
-    img: "/temporary/drop3.png",
-    price: 24.9,
-    options: [
-      {
-        title: "Small",
-        additionalPrice: 0,
-      },
-      {
-        title: "Medium",
-        additionalPrice: 4,
-      },
-      {
-        title: "Large",
-        additionalPrice: 6,
-      },
-    ],
-  },
-  {
-    id: 4,
-    title: "tbd",
-    desc: "Ignite your taste buds with this fiery pasta creation, combining penne in a spicy tomato sauce infused with garlic, red chili flakes, and fresh basil for the ultimate comfort food experience.",
-    img: "/temporary/drop4.png",
-    price: 26.9,
-    options: [
-      {
-        title: "Small",
-        additionalPrice: 0,
-      },
-      {
-        title: "Medium",
-        additionalPrice: 4,
-      },
-      {
-        title: "Large",
-        additionalPrice: 6,
-      },
-    ],
-  },
-  {
-    id: 5,
-    title: "tbd",
-    desc: "Ignite your taste buds with a fiery kick! This burger features a succulent beef patty, fiery jalapeños, pepper jack cheese, and a zesty chipotle mayo sauce , and all the classic fixings on a toasted bun.",
-    img: "/temporary/drop5.png",
-    price: 29.9,
-    options: [
-      {
-        title: "Small",
-        additionalPrice: 0,
-      },
-      {
-        title: "Medium",
-        additionalPrice: 4,
-      },
-      {
-        title: "Large",
-        additionalPrice: 6,
-      },
-    ],
-  },
-  {
-    id: 6,
-    title: "TBD",
-    desc: "TBD",
-    img: "/temporary/drop5.png",
-    price: 24.9,
-    options: [
-      {
-        title: "Small",
-        additionalPrice: 0,
-      },
-      {
-        title: "Medium",
-        additionalPrice: 4,
-      },
-      {
-        title: "Large",
-        additionalPrice: 6,
-      },
-    ],
-  },
-  {
-    id: 6,
-    title: "TBD",
-    desc: "TBD",
-    img: "/temporary/drop4.png",
-    price: 24.9,
-    options: [
-      {
-        title: "Small",
-        additionalPrice: 0,
-      },
-      {
-        title: "Medium",
-        additionalPrice: 4,
-      },
-      {
-        title: "Large",
-        additionalPrice: 6,
-      },
-    ],
-  },
-  {
-    id: 6,
-    title: "TBD",
-    desc: "TBD",
-    img: "/temporary/drop2.png",
-    price: 24.9,
-    options: [
-      {
-        title: "Small",
-        additionalPrice: 0,
-      },
-      {
-        title: "Medium",
-        additionalPrice: 4,
-      },
-      {
-        title: "Large",
-        additionalPrice: 6,
-      },
-    ],
-  },
-  {
-    id: 6,
-    title: "TBD",
-    desc: "TBD",
-    img: "/temporary/drop4.png",
-    price: 24.9,
-    options: [
-      {
-        title: "Small",
-        additionalPrice: 0,
-      },
-      {
-        title: "Medium",
-        additionalPrice: 4,
-      },
-      {
-        title: "Large",
-        additionalPrice: 6,
-      },
-    ],
-  },
-];
-
-
+// export const featuredProducts: Products = [
+//   {
+//     id: 1,
+//     title: "Sicilian",
+//     desc: "Ignite your taste buds with a fiery combination of spicy pepperoni, jalapeños, crushed red pepper flakes, and melted mozzarella cheese, delivering a kick with every bite.",
+//     img: "/temporary/drop1.png",
+//     price: 24.9,
+//     options: [
+//       {
+//         title: "Small",
+//         additionalPrice: 0,
+//       },
+//       {
+//         title: "Medium",
+//         additionalPrice: 4,
+//       },
+//       {
+//         title: "Large",
+//         additionalPrice: 6,
+//       },
+//     ],
+//   },
+//   {
+//     id: 2,
+//     title: "Sicilian",
+//     desc: "Indulge in smoky goodness with a flame-grilled beef patty, topped with crispy bacon, melted cheddar cheese, caramelized onions, and a smattering of tangy BBQ sauce.",
+//     img: "/temporary/drop2.png",
+//     price: 29.9,
+//     options: [
+//       {
+//         title: "Small",
+//         additionalPrice: 0,
+//       },
+//       {
+//         title: "Medium",
+//         additionalPrice: 4,
+//       },
+//       {
+//         title: "Large",
+//         additionalPrice: 6,
+//       },
+//     ],
+//   },
+//   {
+//     id: 3,
+//     title: "tbd",
+//     desc: "A classic Italian delight featuring a thin, crispy crust, tangy tomato sauce, fresh mozzarella, and a medley of aromatic herbs topped with lettuce, tomatoes, and a dollop of tangy mayo.",
+//     img: "/temporary/drop3.png",
+//     price: 24.9,
+//     options: [
+//       {
+//         title: "Small",
+//         additionalPrice: 0,
+//       },
+//       {
+//         title: "Medium",
+//         additionalPrice: 4,
+//       },
+//       {
+//         title: "Large",
+//         additionalPrice: 6,
+//       },
+//     ],
+//   },
+//   {
+//     id: 4,
+//     title: "tbd",
+//     desc: "Ignite your taste buds with this fiery pasta creation, combining penne in a spicy tomato sauce infused with garlic, red chili flakes, and fresh basil for the ultimate comfort food experience.",
+//     img: "/temporary/drop4.png",
+//     price: 26.9,
+//     options: [
+//       {
+//         title: "Small",
+//         additionalPrice: 0,
+//       },
+//       {
+//         title: "Medium",
+//         additionalPrice: 4,
+//       },
+//       {
+//         title: "Large",
+//         additionalPrice: 6,
+//       },
+//     ],
+//   },
+//   {
+//     id: 5,
+//     title: "tbd",
+//     desc: "Ignite your taste buds with a fiery kick! This burger features a succulent beef patty, fiery jalapeños, pepper jack cheese, and a zesty chipotle mayo sauce , and all the classic fixings on a toasted bun.",
+//     img: "/temporary/drop5.png",
+//     price: 29.9,
+//     options: [
+//       {
+//         title: "Small",
+//         additionalPrice: 0,
+//       },
+//       {
+//         title: "Medium",
+//         additionalPrice: 4,
+//       },
+//       {
+//         title: "Large",
+//         additionalPrice: 6,
+//       },
+//     ],
+//   },
+//   {
+//     id: 6,
+//     title: "TBD",
+//     desc: "TBD",
+//     img: "/temporary/drop5.png",
+//     price: 24.9,
+//     options: [
+//       {
+//         title: "Small",
+//         additionalPrice: 0,
+//       },
+//       {
+//         title: "Medium",
+//         additionalPrice: 4,
+//       },
+//       {
+//         title: "Large",
+//         additionalPrice: 6,
+//       },
+//     ],
+//   },
+//   {
+//     id: 6,
+//     title: "TBD",
+//     desc: "TBD",
+//     img: "/temporary/drop4.png",
+//     price: 24.9,
+//     options: [
+//       {
+//         title: "Small",
+//         additionalPrice: 0,
+//       },
+//       {
+//         title: "Medium",
+//         additionalPrice: 4,
+//       },
+//       {
+//         title: "Large",
+//         additionalPrice: 6,
+//       },
+//     ],
+//   },
+//   {
+//     id: 6,
+//     title: "TBD",
+//     desc: "TBD",
+//     img: "/temporary/drop2.png",
+//     price: 24.9,
+//     options: [
+//       {
+//         title: "Small",
+//         additionalPrice: 0,
+//       },
+//       {
+//         title: "Medium",
+//         additionalPrice: 4,
+//       },
+//       {
+//         title: "Large",
+//         additionalPrice: 6,
+//       },
+//     ],
+//   },
+//   {
+//     id: 6,
+//     title: "TBD",
+//     desc: "TBD",
+//     img: "/temporary/drop4.png",
+//     price: 24.9,
+//     options: [
+//       {
+//         title: "Small",
+//         additionalPrice: 0,
+//       },
+//       {
+//         title: "Medium",
+//         additionalPrice: 4,
+//       },
+//       {
+//         title: "Large",
+//         additionalPrice: 6,
+//       },
+//     ],
+//   },
+// ];
 
 
-export const sandwiches: Products = [
-  {
-    id: 1,
-    title: "tbd",
-    desc: "Ignite your taste buds with a fiery combination of spicy pepperoni, jalapeños, crushed red pepper flakes, and melted mozzarella cheese, delivering a kick with every bite.",
-    img: "/temporary/drop3.png",
-    price: 24.9,
-    options: [
-      {
-        title: "Small",
-        additionalPrice: 0,
-      },
-      {
-        title: "Medium",
-        additionalPrice: 4,
-      },
-      {
-        title: "Large",
-        additionalPrice: 6,
-      },
-    ],
-  },
-  {
-    id: 2,
-    title: "tbd2",
-    desc: "Embark on a culinary journey with this Mediterranean-inspired creation, featuring zesty feta cheese, Kalamata olives, sun-dried tomatoes, and a sprinkle of oregano.",
-    img: "/temporary/drop1.png",
-    price: 32.9,
-    options: [
-      {
-        title: "Small",
-        additionalPrice: 0,
-      },
-      {
-        title: "Medium",
-        additionalPrice: 4,
-      },
-      {
-        title: "Large",
-        additionalPrice: 6,
-      },
-    ],
-  },
-  {
-    id: 3,
-    title: "tbd3",
-    desc: "A classic Italian delight featuring a thin, crispy crust, tangy tomato sauce, fresh mozzarella, and a medley of aromatic herbs topped with lettuce, tomatoes, and a dollop of tangy mayo.",
-    img: "/temporary/drop2.png",
-    price: 26.9,
-    options: [
-      {
-        title: "Small",
-        additionalPrice: 0,
-      },
-      {
-        title: "Medium",
-        additionalPrice: 4,
-      },
-      {
-        title: "Large",
-        additionalPrice: 6,
-      },
-    ],
-  },
-  {
-    id: 4,
-    title: "tbd4",
-    desc: "A classic Italian delight featuring a thin, crispy crust, tangy tomato sauce, fresh mozzarella, and a medley of aromatic herbs topped with lettuce, tomatoes, and a dollop of tangy mayo.",
-    img: "/temporary/drop5.png",
-    price: 28.9,
-    options: [
-      {
-        title: "Small",
-        additionalPrice: 0,
-      },
-      {
-        title: "Medium",
-        additionalPrice: 4,
-      },
-      {
-        title: "Large",
-        additionalPrice: 6,
-      },
-    ],
-  },
-  {
-    id: 5,
-    title: "tbd6",
-    desc: "A classic Italian delight featuring a thin, crispy crust, tangy tomato sauce, fresh mozzarella, and a medley of aromatic herbs topped with lettuce, tomatoes, and a dollop of tangy mayo.",
-    img: "/temporary/drop2.png",
-    price: 24.9,
-    options: [
-      {
-        title: "Small",
-        additionalPrice: 0,
-      },
-      {
-        title: "Medium",
-        additionalPrice: 4,
-      },
-      {
-        title: "Large",
-        additionalPrice: 6,
-      },
-    ],
-  },
-  {
-    id: 6,
-    title: "tbd7",
-    desc: "Experience pure cheesy bliss with a melty blend of mozzarella, cheddar, provolone, and Parmesan cheeses, creating a rich and indulgent pizza experience.",
-    img: "/temporary/drop4.png",
-    price: 22.9,
-    options: [
-      {
-        title: "Small",
-        additionalPrice: 0,
-      },
-      {
-        title: "Medium",
-        additionalPrice: 4,
-      },
-      {
-        title: "Large",
-        additionalPrice: 6,
-      },
-    ],
-  },
-];
-
-export const singleProduct: Product = {
-  id: 1,
-  title: "tbd8",
-  desc: "Ignite your taste buds with a fiery combination of spicy pepperoni, jalapeños, crushed red pepper flakes, and melted mozzarella cheese, delivering a kick with every bite.",
-  img: "/temporary/drop1.png",
-  price: 24.9,
-  options: [
-    {
-      title: "Small",
-      additionalPrice: 0,
-    },
-    {
-      title: "Medium",
-      additionalPrice: 4,
-    },
-    {
-      title: "Large",
-      additionalPrice: 6,
-    },
-  ],
-};
 
 
-type Menu = {
-  id: number;
-  slug: string;
-  title: string;
-  desc?: string;
-  img?: string;
-  color: string;
-}[];
+// export const sandwiches: Products = [
+//   {
+//     id: 1,
+//     title: "tbd",
+//     desc: "Ignite your taste buds with a fiery combination of spicy pepperoni, jalapeños, crushed red pepper flakes, and melted mozzarella cheese, delivering a kick with every bite.",
+//     img: "/temporary/drop3.png",
+//     price: 24.9,
+//     options: [
+//       {
+//         title: "Small",
+//         additionalPrice: 0,
+//       },
+//       {
+//         title: "Medium",
+//         additionalPrice: 4,
+//       },
+//       {
+//         title: "Large",
+//         additionalPrice: 6,
+//       },
+//     ],
+//   },
+//   {
+//     id: 2,
+//     title: "tbd2",
+//     desc: "Embark on a culinary journey with this Mediterranean-inspired creation, featuring zesty feta cheese, Kalamata olives, sun-dried tomatoes, and a sprinkle of oregano.",
+//     img: "/temporary/drop1.png",
+//     price: 32.9,
+//     options: [
+//       {
+//         title: "Small",
+//         additionalPrice: 0,
+//       },
+//       {
+//         title: "Medium",
+//         additionalPrice: 4,
+//       },
+//       {
+//         title: "Large",
+//         additionalPrice: 6,
+//       },
+//     ],
+//   },
+//   {
+//     id: 3,
+//     title: "tbd3",
+//     desc: "A classic Italian delight featuring a thin, crispy crust, tangy tomato sauce, fresh mozzarella, and a medley of aromatic herbs topped with lettuce, tomatoes, and a dollop of tangy mayo.",
+//     img: "/temporary/drop2.png",
+//     price: 26.9,
+//     options: [
+//       {
+//         title: "Small",
+//         additionalPrice: 0,
+//       },
+//       {
+//         title: "Medium",
+//         additionalPrice: 4,
+//       },
+//       {
+//         title: "Large",
+//         additionalPrice: 6,
+//       },
+//     ],
+//   },
+//   {
+//     id: 4,
+//     title: "tbd4",
+//     desc: "A classic Italian delight featuring a thin, crispy crust, tangy tomato sauce, fresh mozzarella, and a medley of aromatic herbs topped with lettuce, tomatoes, and a dollop of tangy mayo.",
+//     img: "/temporary/drop5.png",
+//     price: 28.9,
+//     options: [
+//       {
+//         title: "Small",
+//         additionalPrice: 0,
+//       },
+//       {
+//         title: "Medium",
+//         additionalPrice: 4,
+//       },
+//       {
+//         title: "Large",
+//         additionalPrice: 6,
+//       },
+//     ],
+//   },
+//   {
+//     id: 5,
+//     title: "tbd6",
+//     desc: "A classic Italian delight featuring a thin, crispy crust, tangy tomato sauce, fresh mozzarella, and a medley of aromatic herbs topped with lettuce, tomatoes, and a dollop of tangy mayo.",
+//     img: "/temporary/drop2.png",
+//     price: 24.9,
+//     options: [
+//       {
+//         title: "Small",
+//         additionalPrice: 0,
+//       },
+//       {
+//         title: "Medium",
+//         additionalPrice: 4,
+//       },
+//       {
+//         title: "Large",
+//         additionalPrice: 6,
+//       },
+//     ],
+//   },
+//   {
+//     id: 6,
+//     title: "tbd7",
+//     desc: "Experience pure cheesy bliss with a melty blend of mozzarella, cheddar, provolone, and Parmesan cheeses, creating a rich and indulgent pizza experience.",
+//     img: "/temporary/drop4.png",
+//     price: 22.9,
+//     options: [
+//       {
+//         title: "Small",
+//         additionalPrice: 0,
+//       },
+//       {
+//         title: "Medium",
+//         additionalPrice: 4,
+//       },
+//       {
+//         title: "Large",
+//         additionalPrice: 6,
+//       },
+//     ],
+//   },
+// ];
 
-export const menu: Menu = [
-  {
-    id: 1,
-    slug: "sandwich",
-    title: "sandwiches",
-    desc: "tbd",
-    img: "/temporary/drop1.png",
-    color: "black",
-  },
-  {
-    id: 2,
-    slug: "sides",
-    title: "Delicious Sides",
-    desc: "tbd",
-    img: "/temporary/drop2.png",
-    color: "black",
-  },
-  // {
-  //   id: 3,
-  //   slug: "sandwich",
-  //   title: "Sandwiches",
-  //   desc: "tbd",
-  //   img: "/temporary/drop3.png",
-  //   color: "orange",
-  // },
-];
+// export const singleProduct: Product = {
+//   id: 1,
+//   title: "tbd8",
+//   desc: "Ignite your taste buds with a fiery combination of spicy pepperoni, jalapeños, crushed red pepper flakes, and melted mozzarella cheese, delivering a kick with every bite.",
+//   img: "/temporary/drop1.png",
+//   price: 24.9,
+//   options: [
+//     {
+//       title: "Small",
+//       additionalPrice: 0,
+//     },
+//     {
+//       title: "Medium",
+//       additionalPrice: 4,
+//     },
+//     {
+//       title: "Large",
+//       additionalPrice: 6,
+//     },
+//   ],
+// };
+
+
+// type Menu = {
+//   id: number;
+//   slug: string;
+//   title: string;
+//   desc?: string;
+//   img?: string;
+//   color: string;
+// }[];
+
+// export const menu: Menu = [
+//   {
+//     id: 1,
+//     slug: "sandwich",
+//     title: "sandwiches",
+//     desc: "tbd",
+//     img: "/temporary/drop1.png",
+//     color: "black",
+//   },
+//   {
+//     id: 2,
+//     slug: "sides",
+//     title: "Delicious Sides",
+//     desc: "tbd",
+//     img: "/temporary/drop2.png",
+//     color: "black",
+//   },
+//   // {
+//   //   id: 3,
+//   //   slug: "sandwich",
+//   //   title: "Sandwiches",
+//   //   desc: "tbd",
+//   //   img: "/temporary/drop3.png",
+//   //   color: "orange",
+//   // },
+// ];

05525ba91f98cab18a8626696040e1d7f54d5c1d
“Lissa <lwarshaw674@gmail.com>
2023-09-27 09:16:28 -0400

    updated decimal

--- src/app/api/(checkout)/create-intent/[orderId]/route.ts
+++ src/app/api/(checkout)/create-intent/[orderId]/route.ts
@@ -1,5 +1,6 @@
 import { prisma } from "@/utils/connect";
 import { NextRequest, NextResponse } from "next/server";
+import Decimal from 'decimal.js';
 
 const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
 
@@ -16,28 +17,38 @@ export async function POST(
   });
 
   if (order) {
-    const paymentIntent = await stripe.paymentIntents.create({
-      amount: order.price * 100,
-      currency: "usd",
-      automatic_payment_methods: {
-        enabled: true,
-      },
-    });
-
-    await prisma.order.update({
-      where: {
-        id: orderId,
-      },
-      data: { intent_id: paymentIntent.id },
-    });
-
-    return new NextResponse(
-      JSON.stringify({ clientSecret: paymentIntent.client_secret }),
-      { status: 200 }
-    );
+    // Convert order.price to a number using decimal.js
+    const priceAsDecimal = new Decimal(order.price);
+    
+    // Ensure that priceAsDecimal is a valid number
+    if (!priceAsDecimal.isNaN()) {
+      const amount = priceAsDecimal.times(100).toNumber();
+      
+      const paymentIntent = await stripe.paymentIntents.create({
+        amount,
+        currency: "usd",
+        automatic_payment_methods: {
+          enabled: true,
+        },
+      });
+  
+      await prisma.order.update({
+        where: {
+          id: orderId,
+        },
+        data: { intent_id: paymentIntent.id },
+      });
+  
+      return new NextResponse(
+        JSON.stringify({ clientSecret: paymentIntent.client_secret }),
+        { status: 200 }
+      );
+    }
   }
+  
+  // Handle the case where order.price is not a valid number
   return new NextResponse(
-    JSON.stringify({ message:"Order not found!" }),
-    { status: 404 }
+    JSON.stringify({ message: "Invalid order price!" }),
+    { status: 400 }
   );
 }

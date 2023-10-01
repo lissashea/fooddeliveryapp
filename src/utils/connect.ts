a12f63e73491313ecfa765b57b4371690646f790
“Lissa <lwarshaw674@gmail.com>
2023-10-01 11:55:52 -0400

    connect

--- src/utils/connect.ts
+++ src/utils/connect.ts
@@ -8,9 +8,3 @@ export const prisma = globalForPrisma.prisma ?? new PrismaClient()
 
 if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma
 
-if (!process.env.DATABASE_URL) {
-  console.error('DATABASE_URL is not defined in your environment.');
-  process.exit(1); // Exit the application with an error code
-}
-
-console.log('DATABASE_URL:', process.env.DATABASE_URL);

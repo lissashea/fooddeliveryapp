64ecb78d2269ce1412ceb0904bf304096f89bf56
“Lissa <lwarshaw674@gmail.com>
2023-10-01 11:54:15 -0400

    env

--- src/utils/connect.ts
+++ src/utils/connect.ts
@@ -1,14 +1,16 @@
-import { PrismaClient } from '@prisma/client';
+import { PrismaClient } from '@prisma/client'
 
-// Define the database URL for development and production environments
-const devDatabaseUrl = 'postgresql://myuser:password@localhost:5432/mydb?schema=public';
-const prodDatabaseUrl = process.env.DATABASE_URL || 'default-production-url';
+const globalForPrisma = globalThis as unknown as {
+  prisma: PrismaClient | undefined
+}
 
-// Create a Prisma client instance
-export const prisma = new PrismaClient({
-  datasources: {
-    db: {
-      url: process.env.NODE_ENV === 'production' ? prodDatabaseUrl : devDatabaseUrl,
-    },
-  },
-});
+export const prisma = globalForPrisma.prisma ?? new PrismaClient()
+
+if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma
+
+if (!process.env.DATABASE_URL) {
+  console.error('DATABASE_URL is not defined in your environment.');
+  process.exit(1); // Exit the application with an error code
+}
+
+console.log('DATABASE_URL:', process.env.DATABASE_URL);

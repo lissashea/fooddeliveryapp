98abbf2c3b8f2d9a70b0a9c9cfe8873996a08c48
“Lissa <lwarshaw674@gmail.com>
2023-10-07 08:27:51 -0400

    s

--- src/app/menu/page.tsx
+++ src/app/menu/page.tsx
@@ -28,7 +28,7 @@ const MenuPage = async () => {
   // console.log("menu: ");
   // console.log(menu);
   
-  // const menu: MenuType = []
+  const menu: MenuType = []
   return (
     <div className="p-4 lg:px-20 xl:px-40 h-[calc(100vh-6rem)] md:h-[calc(100vh-9rem)] flex flex-col md:flex-row items-center">
       {menu.map((category) => (
